8105524b708dfa213e9b5ff2cbe3d05cfc2b5ab4
Iantra Solari <iantrasolari@gmail.com>
2019-01-14 21:30:12 +0100

    Update image_capturer.cpp

--- src/image_capturer.cpp
+++ src/image_capturer.cpp
@@ -71,7 +71,7 @@ void CaptureImageThread( cv::Mat *capture,
 			else
 				frameNum = 0;
 			cv::Mat newimage;
-			cvSetCaptureProperty( cap, cv::CV_CAP_PROP_POS_FRAMES, frameNum );
+			cvSetCaptureProperty( cap, CV_CAP_PROP_POS_FRAMES, frameNum );
     			cap >> newimage;
 			/*Camera.grab();
 			Camera.retrieve( newimage );
