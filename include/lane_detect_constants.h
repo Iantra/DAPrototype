843ea1baedf5c8473681d1ac9dcee9cd96006ead
Iantra Solari <iantrasolari@gmail.com>
2019-01-15 11:32:45 +0100

    Made them actual constants

--- include/lane_detect_constants.h
+++ include/lane_detect_constants.h
@@ -21,13 +21,13 @@
 /*****************************************************************************************/
 namespace lanedetectconstants {
 	//Default polygon
-	Polygon defaultpolygon{ cv::Point(0,0),
+	const Polygon defaultpolygon{ cv::Point(0,0),
 							cv::Point(0,0),
 							cv::Point(0,0),
 							cv::Point(0,0) };
 							
 	//ROI											//Relative to image size, must change
-	std::vector<std::vector<cv::Point>> k_roipoints{{ cv::Point(0,480),
+	const std::vector<std::vector<cv::Point>> k_roipoints{{ cv::Point(0,480),
 													  cv::Point(0,400),
 													  cv::Point(360,240),
 													  cv::Point(440,240),
@@ -35,33 +35,33 @@ namespace lanedetectconstants {
 													  cv::Point(800,480) }};
 													 
 	//Image evaluation
-	cv::Scalar k_lowerwhitethreshold{ 0, 40, 80 };
-	cv::Scalar k_upperwhitethreshold{ 180, 255, 255 };
-	cv::Scalar k_loweryellowthreshold{ 0, 0, 80 };
-	cv::Scalar k_upperyellowthreshold{ 90, 255, 255 };
-	float k_contrastscalefactor{ 0.75f };
+	const cv::Scalar k_lowerwhitethreshold{ 0, 40, 80 };
+	const cv::Scalar k_upperwhitethreshold{ 180, 255, 255 };
+	const cv::Scalar k_loweryellowthreshold{ 0, 0, 80 };
+	const cv::Scalar k_upperyellowthreshold{ 90, 255, 255 };
+	const float k_contrastscalefactor{ 0.75f };
 	
 	//Line filtering
-	float k_maxvanishingpointangle{ 18.0f };
-	uint16_t k_vanishingpointx{ 266 };				//Relative to image size, must change
-	uint16_t k_vanishingpointy{ 200 };				//Relative to image size, must change
-	uint16_t k_verticallimit{ 280 };				//Relative to image size, must change
-	uint16_t k_rho{ 1 };
-	float k_theta{ CV_PI/22.5 };//0.13962634015f };				//Pi / 22.5
-	uint16_t k_minimumsize{ 35 };//25 };					//Relative to image size, must change
-	uint16_t k_maxlinegap{ 200 };//5 };						//Relative to image size, must change
-	uint16_t k_threshold{ 20 };//30 };						//Relative to image size, must change
+	const float k_maxvanishingpointangle{ 18.0f };
+	const uint16_t k_vanishingpointx{ 266 };				//Relative to image size, must change
+	const uint16_t k_vanishingpointy{ 200 };				//Relative to image size, must change
+	const uint16_t k_verticallimit{ 280 };				//Relative to image size, must change
+	const uint16_t k_rho{ 1 };
+	const float k_theta{ CV_PI/22.5 };//0.13962634015f };				//Pi / 22.5
+	const uint16_t k_minimumsize{ 35 };//25 };					//Relative to image size, must change
+	const uint16_t k_maxlinegap{ 200 };//5 };						//Relative to image size, must change
+	const uint16_t k_threshold{ 20 };//30 };						//Relative to image size, must change
 
 	//Polygon filtering
-	uint16_t k_maxoffsetfromcenter{ 400 };			//Relative to image size, must change
-    uint16_t k_minroadwidth{ 540 };					//Relative to image size, must change
-    uint16_t k_maxroadwidth{ 800 };					//Relative to image size, must change
+	const uint16_t k_maxoffsetfromcenter{ 400 };			//Relative to image size, must change
+    	const uint16_t k_minroadwidth{ 540 };					//Relative to image size, must change
+    	const uint16_t k_maxroadwidth{ 800 };					//Relative to image size, must change
 	
 	//Scoring
-	float k_lowestscorelimit{ -400.0f };			//Relative to image size, must change
-	float k_weightedheightwidth{ 100.0f };			//Relative to image size, must change
-	float k_weightedangleoffset{ -5.0f };
-	float k_weightedcenteroffset{ -1.0f };			//Relative to image size, must change
+	const float k_lowestscorelimit{ -400.0f };			//Relative to image size, must change
+	const float k_weightedheightwidth{ 100.0f };			//Relative to image size, must change
+	const float k_weightedangleoffset{ -5.0f };
+	const float k_weightedcenteroffset{ -1.0f };			//Relative to image size, must change
 
 }
 
